2cca821152ed566d0e2e175fd4cef11333fcd0d0
dineshveera1993 <dinesh.veerabathran@gmail.com>
2026-03-01 22:46:35 +0530

    first commit

--- billing/forms.py
+++ billing/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class BillingForm(forms.Form):
     customer_email = forms.EmailField(label="Customer Email")
     paid_amount = forms.FloatField(
-        label="Cash paid by customer",   # ✅ label shown in UI
+        label="Cash paid by customer",   #  label shown in UI
         required=False
     )    
